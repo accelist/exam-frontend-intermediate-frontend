e81d224970b5b6caada0e3c4113265b73f673080
Samuel Hans <hans.samuelhans@gmail.com>
2024-04-25 19:30:26 +0700

    Added delete(wip)

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -4,21 +4,23 @@ import OrderGridData, { OrderGridDataResponse } from "@/types/Orders/OrderGridDa
 import { Page } from "@/types/Page";
 import { faInfoCircle, faMinusSquare, faPencilSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Col, Divider, Row, Space, Table} from "antd";
+import { Button, Col, Divider, Modal, Row, Space, Table } from "antd";
 import { ColumnsType } from "antd/es/table";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const OrderMainMenuIndex: React.FC = ()=>{
+const OrderMainMenuIndex: React.FC = () => {
     const [page] = useState(1);
     const pageRows = 5;
     const [orders, setOrders] = useState<OrderGridDataResponse[]>();
+    const [modal, contextHolder] = Modal.useModal();
 
-    
+
     /**
      * Data fetching function
      * @param formData 
      */
-    async function fetchOrders(formData: OrderGridData){
+    async function fetchOrders(formData: OrderGridData) {
         const reqInit: RequestInit = {
             headers: { ...DefaultApiRequestHeader },
             method: 'POST',
@@ -31,7 +33,7 @@ const OrderMainMenuIndex: React.FC = ()=>{
         setOrders(orderData);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchOrders({
             "orderId": 0,
             "orderFrom": "",
@@ -41,9 +43,29 @@ const OrderMainMenuIndex: React.FC = ()=>{
             "orderedAt": null,
             "currentPage": page,
             "pageSize": 999
-          });
+        });
     }, [page])
     
+    function onClickDeleteOrder(order: OrderGridDataResponse){
+        modal.confirm({
+            title: 'Confirm Delete',
+            content: 'Are you sure you want to delete order?',
+            okButtonProps: {
+                className: 'bg-red-500 text-white'
+            },
+            okText: 'Yes',
+            onOk: () => onConfirmDeleteOrder(order),
+            cancelText: 'No',
+        });
+    }
+    async function onConfirmDeleteOrder(order: OrderGridDataResponse) {
+        try {
+            await fetch(`/api/be/api/v1/Order/DeleteOrder/${order.orderId}`);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     const orderColumns: ColumnsType<OrderGridData> = [
         {
             title: "No. ",
@@ -61,7 +83,7 @@ const OrderMainMenuIndex: React.FC = ()=>{
             title: "Ordered At", dataIndex: 'orderedAt',
             defaultSortOrder: "descend",
             render: (value: string) => <p>{value}</p>
-            
+
         },
         {
             title: "Quantity", dataIndex: 'quantity',
@@ -77,26 +99,41 @@ const OrderMainMenuIndex: React.FC = ()=>{
                 <Button className="bg-white-500">
                     <FontAwesomeIcon className="text-black" icon={faPencilSquare} />
                 </Button>
-                <Button className="bg-white-500">
+                <Button className="bg-white-500" onClick={()=>onClickDeleteOrder(order)}>
                     <FontAwesomeIcon className="text-black" icon={faMinusSquare} />
                 </Button>
-                
+
             </>
         }
     ];
-    
-    function renderTable(){
 
-        return <Table 
-        dataSource={orders} 
-        columns={orderColumns}
-        pagination={{ 
-            pageSize: pageRows,
-         }}
+    function renderTable() {
+
+        return <Table
+            dataSource={orders}
+            columns={orderColumns}
+            pagination={{
+                pageSize: pageRows,
+            }}
         />
     }
 
     return <Space direction="vertical">
+        {contextHolder}
+        <Row>
+
+            <Col>
+            <Row>
+                <Link href='/orders/create'>Create Order</Link>
+            </Row>
+            <Row>
+                <Link href='/authentication/login'>Logout</Link>
+            </Row>
+
+            
+            </Col>
+
+        </Row>
         <Divider>Order Grid Table</Divider>
         <Row>
             <Col>
@@ -109,7 +146,7 @@ const OrderMainMenuIndex: React.FC = ()=>{
 const OrderMainMenuPage: Page = () => {
     return (
         <>
-            <OrderMainMenuIndex/>
+            <OrderMainMenuIndex />
         </>
     );
 }
